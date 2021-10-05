41a4e04710355c2fb5162686508c0f3db843bd8f
unknown <piyushjb7@hotmail.com>
2021-10-05 12:33:48 +0530

    addedloader

--- source/screen/AuthFlow/TopicSelectionScreen.js
+++ source/screen/AuthFlow/TopicSelectionScreen.js
@@ -19,94 +19,103 @@ import { useState } from 'react'
 const {width, height} = Dimensions.get('window')
 
 const TopicSelectionPage = (props) => {
-    // const selectedCategory = []
+  // const selectedCategory = []
 
-     const[selectedCategory,setSelectedCategory] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [submitLoading, setSubmitLoading] = useState(false);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        const fetchPost = async() => {
-            await dispatch(getCategory());
-        }
-        fetchPost()
-    },[])
+  useEffect(() => {
+    const fetchPost = async () => {
+      setLoading(true);
+      await dispatch(getCategory());
+      setLoading(false);
+    };
+    fetchPost();
+  }, []);
 
-    const onSelect = (postId,item) => {
-        // console.log(postId)
-        if(selectedCategory.some(x => x === postId)){
-            console.log('it consist !')
-            const array = selectedCategory.filter((x) => x !== postId);
-            setSelectedCategory(array)
-             
-             console.log("----->", selectedCategory);
-        }else{
-            setSelectedCategory([...selectedCategory,item.id]);
-            console.log("----->", selectedCategory);
-        }
-        
+  const onSelect = (postId, item) => {
+    // console.log(postId)
+    if (selectedCategory.some((x) => x === postId)) {
+      console.log("it consist !");
+      const array = selectedCategory.filter((x) => x !== postId);
+      setSelectedCategory(array);
+
+      console.log("----->", selectedCategory);
+    } else {
+      setSelectedCategory([...selectedCategory, item.id]);
+      console.log("----->", selectedCategory);
     }
+  };
 
-    const submitCategory = async() => {
-        //for mock we are using all the categories, cause we dont have enough post type for each :)
-        //remove temp_cat and replace it with selectedCategory this if we have enough posts
-        const temp_cat = [1, 7, 3, 6, 2, 8, 4];
-         await dispatch(chooseCategory(temp_cat))
-         props.navigation.navigate('Main')
-    }
+  const submitCategory = async () => {
+    //for mock we are using all the categories, cause we dont have enough post type for each :)
+    //remove temp_cat and replace it with selectedCategory this if we have enough posts
+    const temp_cat = [1, 7, 3, 6, 2, 8, 4];
+    setSubmitLoading(true);
+    await dispatch(chooseCategory(temp_cat));
+    setSubmitLoading(false);
+    props.navigation.navigate("Main");
+  };
 
+  const state = useSelector((x) => x.post.allCategories);
+  const ooo = ["i", "l"];
+  //  console.log(state)
 
-     const state = useSelector((x) => x.post.allCategories);
-     const ooo = ['i','l']
-    //  console.log(state)
-
-
-    return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: colors.secondary }}>
-        <LottieView
-          autoPlay={true}
-          speed={1}
-          source={lottieAnimation.background_bubble}
-        />
-        <HeaderComponent
-          leftIcon={() => (
-            <Ionicons
-              style={{ alignSelf: "center" }}
-              name="arrow-back-outline"
-              size={24}
-              color={"#ffffff"}
-            />
-          )}
-          rightIcon={() => (
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.secondary }}>
+      <LottieView
+        autoPlay={true}
+        speed={1}
+        source={lottieAnimation.background_bubble}
+      />
+      <HeaderComponent
+        leftIcon={() => (
+          <Ionicons
+            style={{ alignSelf: "center" }}
+            name="arrow-back-outline"
+            size={24}
+            color={"#ffffff"}
+          />
+        )}
+        rightIcon={() =>
+          !submitLoading ? (
             <Ionicons
               style={{ alignSelf: "center" }}
               name="checkmark-done-circle"
-              size={35}
+              size={24}
               color={
                 selectedCategory.length === 0 ? "#ffffff" : colors.primaryLight
               }
-              onPress={ 
-                selectedCategory.length === 0 ? null : submitCategory 
-              }
+              onPress={selectedCategory.length === 0 ? null : submitCategory}
             />
-          )}
-          headerStyles={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            backgroundColor: colors.black,
-            height: 80,
-            paddingHorizontal: width * 0.035,
-          }}
-          title="Choose Your Topic"
-          titleStyle={{
-            color: "#ffffff",
-            fontFamily: "regular",
-            justifyContent: "center",
-            alignSelf: "center",
-            fontSize: 24,
-          }}
-        />
-        <View style={{ flex: 1 }}>
+          ) : (
+            <ActivityIndicator
+              style={{ alignSelf: "center" }}
+              size="small"
+              color={colors.primaryLight}
+            />
+          )
+        }
+        headerStyles={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          height: 80,
+          paddingHorizontal: width * 0.035,
+        }}
+        title="Choose Your Topic"
+        titleStyle={{
+          color: "#ffffff",
+          fontFamily: "regular",
+          justifyContent: "center",
+          alignSelf: "center",
+          fontSize: 24,
+        }}
+      />
+      <View style={{ flex: 1 }}>
+        {!loading ? (
           <FlatList
             data={state}
             keyExtractor={(item, index) => index.toString()}
@@ -114,14 +123,20 @@ const TopicSelectionPage = (props) => {
               return <TopicCard item={item} onSelect={onSelect} />;
             }}
           />
-          {/* <Button
+        ) : (
+          <ActivityIndicator
+            style={{ alignSelf: "center", justifyContent: "center", flex: 1 }}
+            size="large"
+            color={"#ffffff"}
+          />
+        )}
+        {/* <Button
           title="Floating Button"
 
         /> */}
-        </View>
-      </SafeAreaView>
-    );
-
+      </View>
+    </SafeAreaView>
+  );
 }
 
 
