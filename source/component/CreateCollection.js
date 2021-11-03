89ae40698f59201b2d91f3ea95fbf94b7527aabe
Piyush Joshi <piyushjb7@hotmail.com>
2021-11-03 19:11:56 +0530

    collection and other things

--- source/component/CreateCollection.js
+++ source/component/CreateCollection.js
@@ -1,38 +1,88 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Dimensions, Button, ActivityIndicator, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+  Button,
+  ActivityIndicator,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { colors } from "../Constants/theme";
 import CreatePostStyles from "./Styles/CreatePostStyles";
 import YoutubePost from "./YoutubePost";
+import { Modal } from "react-native-paper";
+
+//actions
+import { createCollection } from "../../store/actions/postCreation";
+import { getCollections } from "../../store/actions/postCreation";
+
 
 const { width, height } = Dimensions.get("window");
+
 
 const CreateCollection = (props) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [titleBorder, setTitleBorder] = useState("black");
   const [desBorder, setDesBorder] = useState("black");
-  const [selectedPost,setSelectedPost] = useState([])
-  const[postLoader,setPostLoader] = useState(false);
+  const [selectedPost, setSelectedPost] = useState([]);
+  const [postLoader, setPostLoader] = useState(false);
+  const [isPreview, setIsPreview] = useState(false);
+  const [previewData,setPreviewData] = useState();
 
   const closeModal = () => {
-    props.onClose()
+    props.onClose();
     console.log(selectedPost);
+  };
+
+  const onHold = (data) => {
+    setIsPreview(true)
+    console.log('aaye',data)
+    setPreviewData(data)
   }
+  const onHoldOut = () => {
+    setIsPreview(false);
+  }
+
+  const createCollections = async () => {
+    if (
+      title.trim().length > 0 &&
+      description.trim().length > 0 &&
+      selectedPost.length !== 0
+    ) {
+      const response = await createCollection(title, description, selectedPost);
+      await getCollections();
+      console.log(response);
+      closeModal();
+    } else {
+      if (title.trim().length === 0) {
+        setTitleBorder("red");
+      }
+      if (description.trim().length === 0) {
+        setDesBorder("red");
+      } else {
+        Alert.alert("", "Please select the posts for this collection", [
+          { text: "OK" },
+        ]);
+      }
+    }
+  };
 
   const onSelect = (id) => {
-    if( selectedPost.some((x) => x === id)){
-      console.log('already in');
+    if (selectedPost.some((x) => x === id)) {
+      console.log("already in");
       const array = selectedPost.filter((x) => x !== id);
-      setSelectedPost(array)
+      setSelectedPost(array);
 
-      console.log('--->',selectedPost);
-    }else{
-      setSelectedPost([...selectedPost,id]);
-      console.log('--->',selectedPost)
+      console.log("--->", selectedPost);
+    } else {
+      setSelectedPost([...selectedPost, id]);
+      console.log("--->", selectedPost);
     }
-    
-  }
+  };
 
   return (
     <View style={{ padding: 10 }}>
@@ -106,13 +156,16 @@ const CreateCollection = (props) => {
         ></View>
         <FlatList
           numColumns={2}
-          data={["1", "2", "3"]}
+          data={props.saved_posts}
           renderItem={({ item, index }) => (
             <YoutubePost
               contentSmall={true}
               data={item}
               index={index}
               onSelect={onSelect}
+              onHold={onHold}
+              onHoldOut={onHoldOut}
+              holdModal={false}
             />
           )}
         />
@@ -128,7 +181,8 @@ const CreateCollection = (props) => {
             borderColor: colors.primaryLight,
             borderWidth: 1,
           }}
-          onPress={closeModal}
+          onPress={createCollections}
+          disabled={postLoader}
         >
           {postLoader ? (
             <ActivityIndicator size="small" color={colors.primaryLight} />
@@ -139,6 +193,15 @@ const CreateCollection = (props) => {
           )}
         </TouchableOpacity>
       </View>
+      <Modal visible={isPreview}>
+        {previewData ? (
+          <YoutubePost
+            holdModal={true}
+            contentSmall={false}
+            data={previewData}
+          />
+        ) : null}
+      </Modal>
     </View>
   );
 };
